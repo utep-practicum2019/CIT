c970733eca6dd5165c644a1f8a9c7539f988d950
hcervantesjr95 <47658270+hcervantesjr95@users.noreply.github.com>
2019-02-16 12:04:43 -0700

    Added Platform Manager

---
+++ PlatformManager/PythonManager.py
@@ -0,0 +1,39 @@
+import sys
+import os 
+
+class PlatfromManager:
+    
+    def __init__(self, args, kwargs):
+        print("Loading plugins")
+    
+    """
+        adds a platform to the plugin manager
+
+        pre: a  directory that contains plugin files
+        post:  
+    """
+    def addPlugin(self):
+        print("adding plugin")
+     
+    def removePlugin(self):
+        print("removing plugin")
+
+    def configureService(self):
+        print("configuring service")
+    
+    def startService(self):
+        print("starting a service")
+    
+    def stopService(self):
+        print("stopping a service")
+    
+    def showService(self):
+        print("showing services")
+
+    def associate_User_Service(self):
+        print("Associating a user to a service")
+    
+
+
+
+   
