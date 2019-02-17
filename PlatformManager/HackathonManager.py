8a932cc411094b86fa58caf85766e01e78eb1ade
nadiakarichev <47723395+nadiakarichev@users.noreply.github.com>
2019-02-17 15:52:28 -0700

    Add files via upload

---
+++ PlatformManager/HackathonManager.py
@@ -0,0 +1,44 @@
+import sys
+import os
+from yapsy.PluginManager import PluginManager
+import abc
+from .PlatformManager import PlatformManager
+
+""" 
+        @authors:
+            Alejandro Balderrama
+            Nadia Karichev
+            Hector Cervantes
+        @description
+            This class represents the platfrom manager. 
+            The plugin manager will be able to add, delete, start, stop, and configure services(platfroms).
+    """
+
+class HackathonManager(PlatformManager):
+
+    def addPlatform(self):
+        print("adding plugin")
+
+    def removePlatform(self):
+        print("removing plugin")
+
+    def configurePlatform(self):
+        print("configuring service")
+
+    def startPlatform(self):
+        print("starting a service")
+
+    def stopPlatform(self):
+        print("stopping a service")
+
+    def showPlatforms(self):
+        print("showing services")
+
+
+platformManager = PlatformManager()
+platformManager.addPlatform()
+platformManager.removePlatform()
+platformManager.configurePlatform()
+platformManager.showPlatforms()
+platformManager.startPlatform()
+platformManager.stopPlatform()
