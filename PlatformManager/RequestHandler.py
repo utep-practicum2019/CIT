19248537e93ff08184d16701a5bdc45ae635a593
nadiakarichev <47723395+nadiakarichev@users.noreply.github.com>
2019-02-17 16:05:29 -0700

    Add files via upload

---
+++ PlatformManager/RequestHandler.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from yapsy.PluginManager import PluginManager
+import abc
+
+
+""" 
+        @authors:
+            Alejandro Balderrama
+            Nadia Karichev
+            Hector Cervantes
+        @description
+            This class represents the platform manager. 
+            The plugin manager will be able to add, delete, start, stop, and configure services(platfroms).
+    """
+
+class RequestHandler():
+    pass
