237a829dc811d5e98cc15ac1159cfd4681f176ad
Abner <apalomino2@miners.utep.edu>
2019-03-02 11:51:39 -0700

    modified to conform with the revised API

--- AccountManager/group_manager.py
+++ AccountManager/group_manager.py
@@ -1,6 +1,8 @@
 from group import Group
 import requests
- 
+
+# TODO: check where the request will be made groupsWJson or groups/id
+
 class GroupManager:
     cit_url = "http://127.0.0.1:5000"
     groups_path = "/api/v2/database/groups"
@@ -26,34 +28,69 @@ class GroupManager:
             group_data = {'group_id': group_id, **kwargs}
             r = requests.post(GroupManager.groups_url + '/' + group_id, json=group_data)
             if r.status_code == requests.codes.ok:
-                return Group(group_id, **kwargs)
+                return True
         # group_id is already used or request failed
-        return None
+        return False
 
     @staticmethod
     def delete_group(group_id):
-        return None
+        # TODO: test group deletion
+        r = requests.delete(GroupManager.groups_url + '/' + group_id)
+        if r.status_code == requests.codes.ok:
+            return True
+        return False
 
     @staticmethod
     def add_user(group_id, username):
-        return None
+        # TODO: test user addition
+        group_data = {'group_id':group_id, 'users':username}
+        r = requests.post(GroupManager.groups_url + '/' + group_id, json=group_data)
+        if r.status_code == requests.codes.ok:
+            return True
+        return False
 
     @staticmethod
     def remove_user(group_id, username):
-        return None
+        # TODO: test user removal
+        group_data = {'group_id':group_id, 'username':username}
+        # TODO:  what does the URL look for user deletion from group?
+        r = requests.delete(GroupManager.groups_url + '/' + group_id, json=group_data)
+        if r.status_code == requests.codes.ok:
+            return True
+        return False
 
     @staticmethod
     def add_platform(group_id, platform):
-        return None
+        # TODO: test platform addition
+        group_data = {'group_id':group_id, 'platforms':platform}
+        r = requests.put(GroupManager.groups_url + '/' + group_id, json=group_data)
+        if r.status_code == requests.codes.ok:
+            return True
+        return False
 
     @staticmethod
     def remove_platform(group_id, platform):
-        return None
+        # TODO: test platform removal
+        group_data = {'group_id':group_id, 'platforms':platform}
+        r = requests.delete(GroupManager.groups_url + '/' + group_id, json=group_data)
+        if r.status_code == requests.codes.ok:
+            return True
+        return False
 
     @staticmethod
     def add_chat(group_id, chat_id):
-        return None
+        # TODO: test chat addition
+        group_data = {'group_id':group_id, 'chat_id':chat_id}
+        r = requests.put(GroupManager.groups_url + '/' + group_id, json=group_data)
+        if r.status_code == requests.codes.ok:
+            return True
+        return False
 
     @staticmethod
     def remove_chat(group_id):
-        return None
+        # TODO: test chat removal
+        group_data = {'group_id':group_id, 'chat_id':None}
+        r = requests.out(GroupManager.groups_url + '/' + group_id, json=group_data)
+        if r.status_code == requests.codes.ok:
+            return True
+        return False
--- AccountManager/user_manager.py
+++ AccountManager/user_manager.py
@@ -3,50 +3,55 @@ import requests
 
 class UserManager:
     cit_url = "http://127.0.0.1:5000"
-    users_path = "/api/v2/database/users"
-    users_url = cit_url + users_path
+    database_path = "/api/v2/database/"
+    database_url = cit_url + database_path
 
     @staticmethod
     def get_user(username):
         # Get info from database
-        user = requests.get(UserManager.users_url + '/' + username)
+        user_data = {'username':username}
+        user = requests.get(UserManager.database_url, json=user_data)
         user_js = user.json()
         return User(**user_js)
 
     @staticmethod
     def get_users():
-        users = requests.get(UserManager.users_url)
+        users = requests.get(UserManager.database_url)
         users_js = users.json()
         return users_js
 
     @staticmethod
-    def create_user(username, password, **kwargs):
+    def create_user(username=None, **kwargs):
         # TODO: test user creation
+        if username is None:
+            # TODO: generate random username
+            username = 'user'
+
         # check if user exists
         if UserManager.get_user(username) is None:
-            user_data = {'username':username, 'password':password, **kwargs}
-            r = requests.post(UserManager.users_url + '/' + username, json=user_data)
-            if r.status_code == requests.codes.ok:
-                return User(username, password, **kwargs)
-        # user already exists or request failed
-        return None
+            user = User(username, **kwargs)
+            r = requests.post(UserManager.database_url, data=user.toJSON())
+            if r.status_code != requests.codes.ok:
+                return False
+            # TODO: make request to connection manager for password
+            return True
+        # user already exists or get failed
+        return False
     
     @staticmethod
     def delete_user(username):
-        return None
+        # TODO: test user deletion
+        user_data = {'username':username}
+        r = requests.delete(UserManager.database_url, json=user_data)
+        if r.status_code == requests.codes.ok:
+            return True
+        return False
     
     @staticmethod
-    def set_group(username, group_id):
-        return None
-
-    @staticmethod
-    def remove_group(username):
-        return None
-
-    @staticmethod
-    def set_internal_ip(username, internal_ip):
-        return None
-
-    @staticmethod
-    def remove_internal_ip(username):
-        return None
+    def update_user(username, updated_user):
+        # TODO: test group association
+        user_data = {'username':username, 'updated_user':{**updated_user}}
+        r = requests.put(UserManager.database_url, json=user_data)
+        if r.status_code == requests.codes.ok:
+            return True
+        return False
