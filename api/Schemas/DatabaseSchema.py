b182c1f8bd8f59164fd5ea645f57a593cc10b9cc
Jose Cabrera <jacabreramaynez@miners.utep.edu>
2019-03-04 20:28:00 -0700

    Add files via upload

---
+++ api/Schemas/DatabaseSchema.py
@@ -0,0 +1,27 @@
+from marshmallow import (Schema, validates_schema, fields)
+
+from Schemas.Group_Schema import GroupSchema
+from Schemas.User_Schema import UserSchema
+
+
+class DatabaseDocumentSchema(Schema):
+    group = fields.Nested(GroupSchema)
+    user = fields.Nested(UserSchema)
+
+    @validates_schema
+    def validate_at_least_one(self, data):
+        if 'group' not in data and 'user' not in data:
+            return 'Server could not complete your request'
+
+
+class DatabaseRequestSchema(Schema):
+    collection_name = fields.String(required=True)
+    document_id = fields.Integer(required=True)
+
+
+class DatabaseModifySchema(DatabaseRequestSchema):
+    document = fields.Nested(DatabaseDocumentSchema, required=True)
+
+
+class DatabaseResponseSchema(Schema):
+    success = fields.Boolean()
--- api/Schemas/Platform_Schema.py
+++ api/Schemas/Platform_Schema.py
@@ -1,11 +1,13 @@
 from marshmallow import Schema, fields
 
+
 # Used for Creating a Platform
 class PlatformCreateRequestSchema(Schema):
     platform_name = fields.String(required=True)
     file_path = fields.String(required=True)
     ip_address = fields.String(required=True)
     port = fields.Int(required=True)
+
 
 # Used for Modifying or Deleting a Platform
 class PlatformRequestSchema(Schema):
@@ -16,7 +18,7 @@ class PlatformRequestSchema(Schema):
     command = fields.String()
     platform_id = fields.Int(required=True)
 
+
 # Used for Responses
 class PlatformResponseSchema(Schema):
     success = fields.Bool()
-
