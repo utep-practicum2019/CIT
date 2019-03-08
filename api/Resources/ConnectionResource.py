0ae76dd5eb0231426a7f76b3885797aaa28ee267
Jose Cabrera <jacabreramaynez@miners.utep.edu>
2019-03-08 14:46:35 -0700

    Add files via upload

--- api/Resources/ConnectionResource.py
+++ api/Resources/ConnectionResource.py
@@ -2,7 +2,7 @@
 from flask import request
 from flask_restful import Resource
 
-from CIT_API_Schema import *
+from Schemas.Connection_Schema import *
 
 
 class ConnectionAPI(Resource):
@@ -11,50 +11,49 @@ class ConnectionAPI(Resource):
         json_data = request.get_json(force=True)
         if not json_data:
             return {'message': 'No input data provided'}, 400
-        data, errors = connection_request_schema.load(json_data)
+        data, errors = connection_post_request_schema.load(json_data)
         if errors:
             return errors, 422
-        if 'num_users' in data:
-            # results = createUserConnection(data['num_users'])
-            results = {
-                'success': True
-            }
-            results = group_response_schema.dump(results)
-            return results
-        else:
-            return {'message': "An appropriate parameter is missing from the request"}, 422
+
+        from ConnectionManager.ConnectionManager import ConnectionManager
+        results = ConnectionManager.addUsers(ConnectionManager(), data["num_users"])
+        results = {"usersDictionary": results}
+        results, errors = connection_post_response_schema.validate(results)
+
+        if errors:
+            return {"success": False}, 503
+
+        return results
 
     def delete(self):
         json_data = request.get_json(force=True)
         if not json_data:
             return {'message': 'No input data provided'}, 400
-        data, errors = connection_request_schema.load(json_data)
+        data, errors = connection_delete_request_schema.load(json_data)
         if errors:
             return errors, 422
-        if 'list_of_users' in data:
-            # results = deleteUserConnection(data['list_of_users'])
-            results = {
-                'success': True
-            }
-            results = group_response_schema.dump(results)
-            return results
+
+        from ConnectionManager.ConnectionManager import ConnectionManager
+        results = ConnectionManager.deleteUsers(ConnectionManager(), data["list_of_users"])
+
+        if results:
+            return {"success": True}
         else:
-            return {'message': "An appropriate parameter is missing from the request"}, 422
+            return {"success": False}, 503
 
     def put(self):
-
         json_data = request.get_json(force=True)
         if not json_data:
             return {'message': 'No input data provided'}, 400
-        data, errors = connection_request_schema.load(json_data)
+        data, errors = connection_post_request_schema.load(json_data)
         if errors:
             return errors, 422
-        if 'username' and 'password' in data:
-            # results = updateUserConnection(data['username'], data['password'])
-            results = {
-                'success': True
-            }
-            results = group_response_schema.dump(results)
-            return results
+
+        from ConnectionManager.ConnectionManager import ConnectionManager
+        results = ConnectionManager.updateUserConnection(ConnectionManager(), data["currUsername"], data["newUsername"],
+                                                         data["newPassword"], data["newIP"])
+
+        if results:
+            return {"success": True}
         else:
-            return {'message': "An appropriate parameter is missing from the request"}, 422
+            return {"success": False}, 503
