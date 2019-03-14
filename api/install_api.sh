43c2d6b791caf18b03f9da4c04afe58ee6c21f1d
mhawkins8811 <mhawkins8811@gmail.com>
2019-03-13 23:30:15 -0500

    Update install_api.sh
    small errors

--- api/install_api.sh
+++ api/install_api.sh
@@ -13,14 +13,14 @@ cd /var/www/cit
 
 sudo apt install python3-venv -y
 sudo pyvenv test_bed_environment
-source test_bed_environment
+source test_bed_environment/bin/activate
 sudo apt-get install python3-pip -y
 sudo chown -R $USER test_bed_environment
 
 pip install flask
 pip install flask-restful
 
-sudo su-c "echo 127.0.0.1 citsystem.com > /etc/hosts"
+sudo su -c "echo 127.0.0.1 citsystem.com > /etc/hosts"
 sudo mv citsystem.com.conf /etc/apache2/sites-available
 sudo a2ensite citsystem.com.conf
 sudo /etc/init.d/apache2 restart
