f67a3c019d7cad4decd4966206f605349da4011e
mhawkins8811 <mhawkins8811@gmail.com>
2019-03-14 00:00:46 -0400

    Add files via upload
    First version of api installer

---
+++ api/install_api.sh
@@ -0,0 +1,25 @@
+sudo apt-get update
+
+
+sudo apt-get install python3
+
+sudo apt-get install apache2 -y
+
+sudo apt-get install libapache2-mod-wsgi-py3 -y
+
+sudo mkdir /var/www/cit
+sudo mv * /var/www/cit
+
+sudo apt install python3-venv -y
+sudo pyvenv test_bed_environment
+source test_bed_environment
+sudo apt-get install python3-pip -y
+sudo chown -R $USER test_bed_environment
+
+pip install flask
+pip install flask-restful
+
+sudo su-c "echo 127.0.0.1 citsystem.com > /etc/hosts"
+sudo mv citsystem.com.conf /etc/apache2/sites-available
+sudo a2ensite citsystem.com.conf
+sudo /etc/init.d/apache2 restart
