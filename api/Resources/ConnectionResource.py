b1157b3fff7f9836bd0d251aeed42d50490a6e28
Jose Cabrera <jacabreramaynez@miners.utep.edu>
2019-03-17 00:36:35 -0600

    Add files via upload

--- api/Resources/ConnectionResource.py
+++ api/Resources/ConnectionResource.py
@@ -7,6 +7,40 @@ from Schemas.Connection_Schema import *
 
 class ConnectionAPI(Resource):
 
+    def __init__(self):
+        from ConnectionManager.ConnectionManager import ConnectionManager
+        self.ConnectionManager = ConnectionManager()
+
+    # To do a get request
+    # Type 1
+    # {
+    #     session_list : Boolean
+    # }
+    def get(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+            return {'message': 'No input data provided'}, 400
+        data, errors = connection_get_request_schema.load(json_data)
+        if errors:
+            return errors, 422
+        results = ""
+        if data["session_list"]:
+            results = self.ConnectionManager.update_session_list()
+            results = {"usersDictionary": results}
+            results, errors = connection_post_response_schema.validate(results)
+            if errors:
+                return {"success": False}, 503
+        return results
+
+    # To do a post request
+    # Type 1
+    # {
+    #     usernames : List[String]
+    # }
+    # Type 2
+    # {
+    #     num_users : Integer
+    # }
     def post(self):
         json_data = request.get_json(force=True)
         if not json_data:
@@ -15,8 +49,12 @@ class ConnectionAPI(Resource):
         if errors:
             return errors, 422
 
-        from ConnectionManager.ConnectionManager import ConnectionManager
-        results = ConnectionManager.addUsers(ConnectionManager(), data["num_users"])
+        results = ""
+        if "usernames" in data:
+            results = self.ConnectionManager.fileAddUsers(data["usernames"])
+        elif "num_users" in data:
+            results = self.ConnectionManager.addUsers(data["num_users"])
+
         results = {"usersDictionary": results}
         results, errors = connection_post_response_schema.validate(results)
 
@@ -25,6 +63,11 @@ class ConnectionAPI(Resource):
 
         return results
 
+    # To do a delete request
+    # Type 1
+    # {
+    #     "list_of_users" : List[String]
+    # }
     def delete(self):
         json_data = request.get_json(force=True)
         if not json_data:
@@ -33,14 +76,29 @@ class ConnectionAPI(Resource):
         if errors:
             return errors, 422
 
-        from ConnectionManager.ConnectionManager import ConnectionManager
-        results = ConnectionManager.deleteUsers(ConnectionManager(), data["list_of_users"])
+        results = self.ConnectionManager.deleteUsers(data["list_of_users"])
 
         if results:
             return {"success": True}
         else:
             return {"success": False}, 503
 
+    # To do a put request:
+    # Type 1
+    # {
+    #     "command" : "start" To start polling thread
+    # }
+    # Type 2
+    # {
+    #     "command" : "stop" To stop polling thread
+    # }
+    # Type 3
+    # {
+    #     "currUsername" : String
+    #     "newUsername" : String
+    #     "newPassword" : String
+    #     "newIP" : String
+    # }
     def put(self):
         json_data = request.get_json(force=True)
         if not json_data:
@@ -49,10 +107,17 @@ class ConnectionAPI(Resource):
         if errors:
             return errors, 422
 
-        from ConnectionManager.ConnectionManager import ConnectionManager
-        results = ConnectionManager.updateUserConnection(ConnectionManager(), data["currUsername"], data["newUsername"],
-                                                         data["newPassword"], data["newIP"])
-
+        if "command" in data:
+            results = True
+            if data["command"] == "start":
+                self.ConnectionManager.pptp_poll_connection()
+            elif data["command"] == "stop":
+                self.ConnectionManager.stop()
+            else:
+                results = False
+        else:
+            results = self.ConnectionManager.updateUserConnection(data["currUsername"], data["newUsername"],
+                                                                  data["newPassword"], data["newIP"])
         if results:
             return {"success": True}
         else:
