b528d04b21bcd925e0daa6d0252bfb31805e578c
Alex <abalderrama5@outlook.com>
2019-03-21 13:11:46 -0600

    Update Platform Manager Testing

--- PlatformManager/Tests/Test_PlatformManager.py
+++ PlatformManager/Tests/Test_PlatformManager.py
@@ -29,16 +29,15 @@ class Test_Tiddlywiki(unittest.TestCase):
         
     def test_wiki_addPlatform(self):
         self.wiki = self.platformManager.createPlatform("TiddlyWiki", {})
-        self.wiki, self.rocketChat = self.platformManager.addPlatform(self.wiki, "Rocket Chat")
+        self.wiki, self.rocketChat = self.platformManager.addPlatform(self.wiki, "RocketChat")
         self.assertTrue(1 <= self.wiki < 100000)
-        self.assertTrue(1 <= self.rocketChat < 100000) 
+        self.assertTrue(1 <= self.rocketChat['RocketChat'] < 100000) 
         #####check return type of addPlatform#####
         
     def test_wiki_createPlatform_with_subs(self):    
-        self.wiki, self.rocketChat = self.platformManager.createPlatform("TiddlyWiki", {"Rocket Chat"})
+        self.wiki, self.rocketChat = self.platformManager.createPlatform("TiddlyWiki", {"RocketChat"})
         self.assertTrue(1 <= self.wiki < 100000)
-        self.assertTrue(1 <= self.rocketChat < 100000)
-        #####check return type of addPlatform#####
+        self.assertTrue(1 <= self.rocketChat["RocketChat"] < 100000)
         
     def test_wiki_deletePlatform(self):
         self.wiki = self.platformManager.createPlatform("TiddlyWiki", {})
@@ -47,14 +46,14 @@ class Test_Tiddlywiki(unittest.TestCase):
         self.assertEqual(self.status, "success")
         
     def test_wiki_startPlatforms(self):
-        self.wiki, self.rocketChat = self.platformManager.createPlatform("TiddlyWiki", {"Rocket Chat"})
-        self.platformManager.startPlatforms(self.wiki, {self.rocketChat})
+        self.wiki, self.rocketChat = self.platformManager.createPlatform("TiddlyWiki", {"RocketChat"})
+        self.platformManager.startPlatforms(self.wiki, {self.rocketChat['RocketChat']})
         #*****incomplete******
         
     def test_wiki_stopPlatforms(self):
-        self.wiki, self.rocketChat = self.platformManager.createPlatform("TiddlyWiki", {"Rocket Chat"})
-        self.platformManager.startPlatforms(self.wiki, {self.rocketChat})
-        self.status = self.platformManager.stopPlatforms(self.wiki, {self.rocketChat})
+        self.wiki, self.rocketChat = self.platformManager.createPlatform("TiddlyWiki", {"RocketChat"})
+        self.platformManager.startPlatforms(self.wiki, {self.rocketChat['RocketChat']})
+        self.status = self.platformManager.stopPlatforms(self.wiki, {self.rocketChat['RocketChat']})
         self.assertEqual(self.status, "Success")
         
     def test_wiki_requestForwarder(self):
@@ -77,37 +76,37 @@ class Test_RocketChat(unittest.TestCase):
         pass
     
     def test_rocketchat_createPlatform(self):
-        self.rocketchat = self.platformManager.createPlatform("Rocket Chat", {})
+        self.rocketchat = self.platformManager.createPlatform("RocketChat", {})
         self.assertTrue(1 <= self.rocketchat < 100000)
         
     def test_rocketchat_addPlatform(self):
-        self.rocketchat = self.platformManager.createPlatform("Rocket Chat", {})
+        self.rocketchat = self.platformManager.createPlatform("RocketChat", {})
         self.rocketchat, self.wiki = self.platformManager.addPlatform(self.rocketchat, "TiddlyWiki")
         self.assertTrue(1 <= self.rocketchat < 100000)
-        self.assertTrue(1 <= self.wiki < 100000) 
+        self.assertTrue(1 <= self.wiki['TiddlyWiki'] < 100000) 
         #####check return type of addPlatform#####
         
     def test_rocketchat_createPlatform_with_subs(self):   
-        self.rocketchat, self.wiki = self.platformManager.createPlatform("Rocket Chat", {"TiddlyWiki"})
+        self.rocketchat, self.wiki = self.platformManager.createPlatform("RocketChat", {"TiddlyWiki"})
         self.assertTrue(1 <= self.rocketchat < 100000)
-        self.assertTrue(1 <= self.wiki < 100000)
+        self.assertTrue(1 <= self.wiki['TiddlyWiki'] < 100000)
         #####check return type of addPlatform##### 
         
     def test_rocketchat_deletePlatform(self):
-        self.rocketchat = self.platformManager.createPlatform("Rocket Chat", {})
+        self.rocketchat = self.platformManager.createPlatform("RocketChat", {})
         self.id, self.status = self.platformManager.deletePlatform(self.rocketchat, {})
         self.assertTrue(1 <= self.id < 100000)
         self.assertEqual(self.status, "success")
         
     def test_rocketchat_startPlatforms(self):
-        self.rocketchat, self.wiki = self.platformManager.createPlatform("Rocket Chat", {"TiddlyWiki"})
-        self.platformManager.startPlatforms(self.rocketchat, {self.wiki})
+        self.rocketchat, self.wiki = self.platformManager.createPlatform("RocketChat", {"TiddlyWiki"})
+        self.platformManager.startPlatforms(self.rocketchat, {self.wiki['TiddlyWiki']})
         #*****incomplete******
         
     def test_rocketchat_stopPlatforms(self):
-        self.rocketchat, self.wiki = self.platformManager.createPlatform("Rocket Chat", {"TiddlyWiki"})
-        self.platformManager.startPlatforms(self.rocketchat, {self.wiki})
-        self.status = self.platformManager.stopPlatforms(self.rocketchat, {self.wiki})
+        self.rocketchat, self.wiki = self.platformManager.createPlatform("RocketChat", {"TiddlyWiki"})
+        self.platformManager.startPlatforms(self.rocketchat, {self.wiki['TiddlyWiki']})
+        self.status = self.platformManager.stopPlatforms(self.rocketchat, {self.wiki['TiddlyWiki']})
         self.assertEqual(self.status, "Success")
         
     def test_rocketchat_requestForwarder(self):
@@ -137,13 +136,13 @@ class Test_Hackathon(unittest.TestCase):
         self.hackathon = self.platformManager.createPlatform("Hackathon", {})
         self.hackathon, self.wiki = self.platformManager.addPlatform(self.hackathon, "TiddlyWiki")
         self.assertTrue(1 <= self.hackathon < 100000)
-        self.assertTrue(1 <= self.wiki < 100000) 
+        self.assertTrue(1 <= self.wiki['TiddlyWiki'] < 100000) 
         #####check return type of addPlatform#####
         
     def test_hackathon_createPlatform_with_subs(self):    
         self.hackathon, self.wiki = self.platformManager.createPlatform("Hackathon", {"TiddlyWiki"})
         self.assertTrue(1 <= self.hackathon < 100000)
-        self.assertTrue(1 <= self.wiki < 100000)
+        self.assertTrue(1 <= self.wiki['TiddlyWiki'] < 100000)
         #####check return type of addPlatform##### 
         
     def test_hackathon_deletePlatform(self):
@@ -154,13 +153,13 @@ class Test_Hackathon(unittest.TestCase):
         
     def test_hackathon_startPlatforms(self):
         self.hackathon, self.wiki = self.platformManager.createPlatform("Hackathon", {"TiddlyWiki"})
-        self.platformManager.startPlatforms(self.hackathon, {self.wiki})
+        self.platformManager.startPlatforms(self.hackathon, {self.wiki['TiddlyWiki']})
         #*****incomplete******
         
     def test_hackathon_stopPlatforms(self):
         self.hackathon, self.wiki = self.platformManager.createPlatform("Hackathon", {"TiddlyWiki"})
-        self.platformManager.startPlatforms(self.hackathon, {self.wiki})
-        self.status = self.platformManager.stopPlatforms(self.hackathon, {self.wiki})
+        self.platformManager.startPlatforms(self.hackathon, {self.wiki['TiddlyWiki']})
+        self.status = self.platformManager.stopPlatforms(self.hackathon, {self.wiki['TiddlyWiki']})
         self.assertEqual(self.status, "Success")
         
     def test_hackathon_requestForwarder(self):
