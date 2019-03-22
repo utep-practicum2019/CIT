446927d19292dbd0df14d1f7c52d79ade0b145d1
Jose Cabrera <jacabreramaynez@miners.utep.edu>
2019-03-21 22:44:37 -0700

    Add files via upload

--- api/CIT_API.py
+++ api/CIT_API.py
@@ -6,7 +6,7 @@ from Resources.ConnectionResource import ConnectionAPI
 from Resources.DatabaseResource import DatabaseAPI
 from Resources.GroupResource import GroupAPI
 from Resources.LoginResource import LoginAPI
-from Resources.PlatformResource import PlatformAPI
+# from Resources.PlatformResource import PlatformAPI
 from Resources.UserResource import UserAPI
 from Resources.VMResource import VMConfigAPI, VMStartAPI, VMStatusAPI, VMSuspendAPI
 
@@ -18,7 +18,7 @@ api.add_resource(DatabaseAPI, '/api/v2/resources/database')
 api.add_resource(ConnectionAPI, '/api/v2/resources/connection')
 api.add_resource(UserAPI, '/api/v2/resources/user')
 api.add_resource(GroupAPI, '/api/v2/resources/group')
-api.add_resource(PlatformAPI, '/api/v2/resources/platform')
+# api.add_resource(PlatformAPI, '/api/v2/resources/platform')
 
 api.add_resource(VMConfigAPI, '/api/v2/resources/vm/manage/config')
 api.add_resource(VMStatusAPI, '/api/v2/resources/vm/manage/status')
--- api/install_api.sh
+++ api/install_api.sh
@@ -3,7 +3,6 @@ sudo apt-get update
 sudo apt-get install python3
 sudo apt-get install apache2 -y
 sudo apt-get install libapache2-mod-wsgi-py3 -y
-sudo apt-get install pptpd -y
 
 sudo mkdir /var/www/cit
 sudo mv * /var/www/cit
@@ -16,27 +15,14 @@ sudo apt-get install python3-pip -y
 sudo chown -R $USER test_bed_environment
 
 
-#install dependencies for request handler
-
-
+pip install pymango
 pip install flask
 
-
-pip install wheel
+#export FLASK_APP=CIT_API
 
 pip install flask-restful
 pip install flask-marshmallow
 pip install flask-httpauth
-pip install flask-api
-pip install pymongo
-pip install pyinotify
-pip install apispec
-pip install apispec-webframeworks
-pip install sockets
-pip install requests
-pip install jsonify
-pip install resource
-pip install schemas
 #flask run
 
 sudo su -c "echo 127.0.0.1 citsystem.com > /etc/hosts"
