126653955725e868f53c792eba282319b6c42d7f
Jose Cabrera <jacabreramaynez@miners.utep.edu>
2019-03-25 00:55:05 -0700

    Add files via upload

--- api/CIT_API.py
+++ api/CIT_API.py
@@ -6,7 +6,8 @@ from Resources.ConnectionResource import ConnectionAPI
 from Resources.DatabaseResource import DatabaseAPI
 from Resources.GroupResource import GroupAPI
 from Resources.LoginResource import LoginAPI
-# from Resources.PlatformResource import PlatformAPI
+from Resources.PlatformResource import PlatformAPI
+from Resources.RocketChatResource import RocketChatAPI
 from Resources.UserResource import UserAPI
 from Resources.VMResource import VMConfigAPI, VMStartAPI, VMStatusAPI, VMSuspendAPI
 
@@ -14,11 +15,14 @@ ma = Marshmallow()
 app = Flask(__name__)
 api = Api(app)
 
+
+
 api.add_resource(DatabaseAPI, '/api/v2/resources/database')
 api.add_resource(ConnectionAPI, '/api/v2/resources/connection')
 api.add_resource(UserAPI, '/api/v2/resources/user')
 api.add_resource(GroupAPI, '/api/v2/resources/group')
-# api.add_resource(PlatformAPI, '/api/v2/resources/platform')
+api.add_resource(PlatformAPI, '/api/v2/resources/platform')
+api.add_resource(RocketChatAPI, '/api/v2/resources/platform/rocketchat')
 
 api.add_resource(VMConfigAPI, '/api/v2/resources/vm/manage/config')
 api.add_resource(VMStatusAPI, '/api/v2/resources/vm/manage/status')
