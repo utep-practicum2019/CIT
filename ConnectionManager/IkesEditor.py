89058bc41f4db2b47419e1bd125a60a825a01c84
Kristian V <kvillanueva1959@gmail.com>
2019-03-28 21:14:50 -0600

    Capability to add users from file

--- ConnectionManager/IkesEditor.py
+++ ConnectionManager/IkesEditor.py
@@ -3,10 +3,10 @@ from Connections import Connections
 
 class IKE_ConnectionsSublcass(Connections):
         def ikesAddUser(self,randomPassword,typeOfAdd):
-                ipsec=open("IkesTest.txt","a")
-                # ipsec=open("/etc/ipsec.secrets","a")
-                nextAvailableUser=super().checkCurUsers("IkesTest.txt")
-                # nextAvailableUser=super().checkCurUsers("/etc/ipsec.secrets")
+                # ipsec=open("IkesTest.txt","a")
+                ipsec=open("/etc/ipsec.secrets","a")
+                # nextAvailableUser=super().checkCurUsers("IkesTest.txt")
+                nextAvailableUser=super().checkCurUsers("/etc/ipsec.secrets")
                 userNumVar=nextAvailableUser[0]
                 # ipEND=nextAvailableUser[1]
                 if(typeOfAdd==True):
