d18ee5f98ca60138bdab10ff610f17e20938fb14
mhawkins8811 <mhawkins8811@gmail.com>
2019-03-31 00:49:35 -0500

    Update install_webserver.sh

--- api/install_webserver.sh
+++ api/install_webserver.sh
@@ -63,5 +63,42 @@ sudo mv citsystem.com.conf /etc/apache2/sites-available
 sudo a2ensite citsystem.com.conf
 sudo /etc/init.d/apache2 restart
 
+#configuring the VPN
+sudo cat >> /etc/pptpd.conf<< END
+localip 10.10.0.1
+remoteip 10.10.0.2-100
+END
+
+sudo cat >> /etc/ppp/pptpd-options<< END
+name pptpd
+ refuse-pap
+ refuse-chap
+ refuse-mschap
+ require-mschap-v2
+ require-mppe-128
+ ms-dns 8.8.8.8
+ #ms-dns 8.8.4.4
+ proxyarp
+ nodefaultroute
+ lock
+ nobsdcomp
+ mu 1490
+ mru 1490
+END
+
+sudo cat >> /etc/ppp/chap-secrets << END
+tempusr pptpd temppass *
+END
+#*allows for all IP
+sudo cat >> /etc/sysctl.conf << END
+net.ipv4.ip_forward=1
+END
+sudo sysctl -p
+
+sudo iptables -t nat -A POSTROUTING -o etho0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o ppp0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i ppp0 -o eth0 -j ACCEPT
+sudo service pptpd restart
+
 echo "CIT_server setup complete."
 echo "Please type citsystem.com in your URL."
