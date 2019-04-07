cc25bb10ad33b087bf6f31e1d20cdf0960bc03a6
mhawkins8811 <mhawkins8811@gmail.com>
2019-04-07 17:34:31 -0600

    Update install_webserver.sh
    update

--- api/install_webserver.sh
+++ api/install_webserver.sh
@@ -58,10 +58,62 @@ sudo apt-get update -y
 sudo apt-get install -y mongodb-org
 #sudo apt-get install -y mongodb-org --allow-unauthenticated
 sudo service mongod start
+
+#configuring the VPN
+sudo su -c" cat >> /etc/pptpd.conf<< END
+localip 10.10.0.1
+remoteip 10.10.0.2-100
+END"
+
+sudo su -c " cat >> /etc/ppp/pptpd-options<< END
+name pptpd
+ refuse-pap
+ refuse-chap
+ refuse-mschap
+ require-mschap-v2
+ require-mppe-128
+ ms-dns 8.8.8.8
+ #ms-dns 8.8.4.4
+ proxyarp
+ nodefaultroute
+ lock
+ nobsdcomp
+ mu 1490
+ mru 1490
+END"
+
+sudo -c " cat >> /etc/ppp/chap-secrets << END
+tempusr pptpd temppass *
+END"
+#*allows for all IP
+sudo -c " cat >> /etc/sysctl.conf << END
+net.ipv4.ip_forward=1
+END"
+sudo sysctl -p
+
+#sudo iptables -t nat -A POSTROUTING -o etho0 -j MASQUERADE
+#sudo iptables -A FORWARD -i eth0 -o ppp0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+#sudo iptables -A FORWARD -i ppp0 -o eth0 -j ACCEPT
+#sudo service pptpd restart
+
 sudo su -c "echo 127.0.0.1 citsystem.com >> /etc/hosts"
 sudo mv citsystem.com.conf /etc/apache2/sites-available
 sudo a2ensite citsystem.com.conf
 sudo /etc/init.d/apache2 restart
 
+#rocketChat portion
+sudo a2enmod proxy proxy_http rewrite
+sudo snap install rocketchat-server
+pip install rocketchat_API
+sudo mv Rocket.Chat.conf /etc/apache2/sites-available
+sudo chmod 644 /etc/apache2/sites-available/Rocket.Chat.conf
+sudo a2ensite Rocket.Chat
+sudo service apache2 reload
+
 echo "CIT_server setup complete."
 echo "Please type citsystem.com in your URL."
+echo "please close terminal once pptpd connection is verified"
+
+sudo systemctl enable pptpd
+sudo systemctl start pptpd
+sudo systemctl status pptpd
