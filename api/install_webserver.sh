6936c48035031412bb140f6ee20ec429576ef334
mhawkins8811 <mhawkins8811@gmail.com>
2019-04-07 17:14:52 -0600

    Update install_webserver.sh

--- api/install_webserver.sh
+++ api/install_webserver.sh
@@ -104,6 +104,7 @@ sudo /etc/init.d/apache2 restart
 #rocketChat portion
 sudo a2enmod proxy proxy_http rewrite
 sudo snap install rocketchat-server
+pip install rocketchat_API
 sudo mv Rocket.Chat.conf /etc/apache2/sites-available
 sudo chmod 644 /etc/apache2/sites-available/Rocket.Chat.conf
 sudo a2ensite Rocket.Chat
