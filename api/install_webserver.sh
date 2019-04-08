a59c8af6484b42c011db0d56922947f8aebed56c
mhawkins8811 <mhawkins8811@gmail.com>
2019-04-07 21:02:10 -0600

    Update install_webserver.sh

--- api/install_webserver.sh
+++ api/install_webserver.sh
@@ -28,6 +28,7 @@ sudo git clone https://github.com/Jermolene/TiddlyWiki5
 sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - 
 sudo apt-get install -y nodejs
 sudo npm install http-server
+sudo npm i http-server
 cd ..
 
 pip install flask
