0eef5778e86b6f6dc635f85a0eca5c2edef93c22
practicum <utep-practicum2019@gmail.com>
2019-05-04 17:08:15 -0600

    got script and conf files from citsystem.com

--- api/configif.sh
+++ api/configif.sh
@@ -1,6 +1,7 @@
 sudo ifconfig enp0s3 down
-sudo ifconfig enp0s3 10.0.0.10/24
+sudo ifconfig enp0s3 129.108.7.11/24
 sudo ifconfig enp0s3 up
-sudo route add default gw 10.0.0.1 enp0s3
+sudo route add default gw 129.108.7.1 enp0s3
 route
 ifconfig
+#"192.168.0.1"
