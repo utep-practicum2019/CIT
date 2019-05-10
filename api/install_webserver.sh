3ee3a305f330edd7767fd0310aa2e84bebe9f1e6
mhawkins8811 <mhawkins8811@gmail.com>
2019-05-10 16:24:09 -0600

    Update install_webserver.sh

--- api/install_webserver.sh
+++ api/install_webserver.sh
@@ -105,7 +105,9 @@ sudo su -c "echo export DISPLAY=$DISPLAY>> /etc/apache2/envvars"
 
 sudo su -c "echo '$CIT_IP citsystem.com' >> /etc/hosts"
 sudo sed -i -e 's/INSERT_IP_HERE/'"$CIT_IP"':80/' citsystem.com.conf
-sudo sed -i -e 's/INSERT_USER_HERE/'"$SUDO_USER"'/' citsystem.com.conf
+#sudo sed -i -e 's/INSERT_USER_HERE/'"$SUDO_USER"'/' citsystem.com.conf
+sudo sed -i -e 's/INSERT_USER_HERE/'"$USER"'/' citsystem.com.conf
+sudo sed -i -e 's/INSERT_USER_HERE/'"$USER"'/' citsystem.com.conf
 sudo mv citsystem.com.conf /etc/apache2/sites-available
 sudo a2ensite citsystem.com.conf
 sudo /etc/init.d/apache2 restart
