eda4d4812609507470f2e8e5511f91da5983bd34
mhawkins8811 <mhawkins8811@gmail.com>
2019-05-13 17:48:09 -0600

    Update install_webserver.sh

--- api/install_webserver.sh
+++ api/install_webserver.sh
@@ -2,7 +2,7 @@
 
 if ["$(id -u)" == "0" ];
 then 
-    echo "install_webserver.sh should be ran as regular user, not sudo."
+    echo "install_webserver.sh should be ran as regular user, not sudo." 1>&2
     exit 1
 fi
 
