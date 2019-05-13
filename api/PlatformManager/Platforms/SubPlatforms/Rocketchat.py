41963c465833cbb8f5e5612b768718c0bcfae1f1
CIT <jacabreramaynez@miners.utep.edu>
2019-05-12 21:58:38 -0600

    Auto stash before merge of "AdminGUI" and "origin/integration"

--- api/PlatformManager/Platforms/SubPlatforms/Rocketchat.py
+++ api/PlatformManager/Platforms/SubPlatforms/Rocketchat.py
@@ -15,7 +15,6 @@ from rocketchat_API.rocketchat import RocketChat
             The plugin manager will be able to start, stop, and configure Chat platform.
     """
 
-
 class Rocketchat(Platform):
     # fill the values here for your specific platform
     platform_name = "Rocketchat"
